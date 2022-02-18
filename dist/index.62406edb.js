f2e6a1f53ff457806d5549ed6d71cc1c9d4f34a8
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-02-18 04:12:54 +0330

    First API call( AJAX call ) on Forkify API
    Create showRecipe asynch function

--- dist/index.62406edb.js
+++ dist/index.62406edb.js
@@ -1,11 +1,31 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 const timeout = function(s) {
     return new Promise(function(_, reject) {
         setTimeout(function() {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+const showRecipe = async function() {
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`);
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    console.log(data);
+    let { recipe  } = data.data;
+    recipe = {
+        cookingTime: recipe.cooking_time,
+        id: recipe.id,
+        image: recipe.image_url,
+        ingredients: recipe.ingredients,
+        publisher: recipe.publisher,
+        servings: recipe.servings,
+        sourceUrl: recipe.source_url,
+        title: recipe.title
+    };
+    console.log(recipe);
+};
+showRecipe();
 
 //# sourceMappingURL=index.62406edb.js.map
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-const recipeContainer = document.querySelector('.recipe');
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +11,30 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  const res = await fetch(
+    `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+  );
+
+  const data = await res.json();
+
+  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+  console.log(data);
+  let { recipe } = data.data;
+  recipe = {
+    cookingTime: recipe.cooking_time,
+    id: recipe.id,
+    image: recipe.image_url,
+    ingredients: recipe.ingredients,
+    publisher: recipe.publisher,
+    servings: recipe.servings,
+    sourceUrl: recipe.source_url,
+    title: recipe.title,
+  };
+
+  console.log(recipe);
+};
+
+showRecipe();
