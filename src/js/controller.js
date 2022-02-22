aa0bfb4d61f987339685789bb8a3308e2bbc378f
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2022-02-22 11:59:58 +0330

    Add config and helpers module files

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,17 +4,7 @@ import recipeViwe from "./viwes/recipeViwe.js";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-const recipeContainer = document.querySelector(".recipe");
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// https://forkify-api.herokuapp.com/v2
+//const recipeContainer = document.querySelector(".recipe");
 
 ///////////////////////////////////////
 
--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from "regenerator-runtime";
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
 
 export const state = {
   recipe: {},
@@ -6,13 +8,7 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}${id}`);
 
     const { recipe } = data.data;
     state.recipe = {
@@ -26,6 +22,6 @@ export const loadRecipe = async function (id) {
       title: recipe.title,
     };
   } catch (error) {
-      alert(error);
+    alert(error);
   }
 };
